1afce5d4b02febc794f10e860849fd3ee8610425
brandon708 <sotomallquibrandon708@gmail.com>
2022-04-19 18:30:24 -0500

    validaciones avanzando

--- src/main/resources/static/resources/js/validarDepa.js
+++ src/main/resources/static/resources/js/validarDepa.js
@@ -1,48 +1,38 @@
 function validarDepa() {
 
-	let nombre = document.getElementById("idnombres").value;
-	let apellido = document.getElementById("idapellidos").value;
-	var dni = document.getElementById("idDni").value;
-	var celular = document.getElementById("idcelular").value;
-	var correo = document.getElementById("idcorreo").value;
-	var password = document.getElementById("idpassword").value;
-	var descripcionRaza = document.getElementById("iddescripcion").value;
+	var habitaciones = document.getElementById("idhabitaciones").value;
+	var cocinas = document.getElementById("idcocinas").value;
+	var lavanderia = document.getElementById("idlavanderia").value;
+	var sala = document.getElementById("idsala").value;
+	var baño = document.getElementById("idbaño").value;
+	let tamaño = document.getElementById("idtamaño").value;
 	
-	var regexemail = /^\w+@\w+\.+[a-z]{2,3}$/;
-	var regexnombre = /^[A-Za-zƒŠŒŽšœžñŸÀÁÂÃÄÅÆÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèé êëìíîïðñòóôõöùúûüýþÿ]*$/;
 
-	if (!nombre || !apellido || !dni || !celular || !correo || !password) {
+	var regextamaño = /^[A-Za-zƒŠŒŽšœžñŸÀÁÂÃÄÅÆÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèé êëìíîïðñòóôõöùúûüýþÿ]*$/;
+
+	if (!habitaciones || !cocinas || !lavanderia || !sala || !baño || !tamaño) {
 		alert("Todos los campos son obligatorios");
 		return false;
-	} else if (nombre.length < 3 || nombre.length >= 30) {
-		alert('El nombre solo acepta como minimo 3 y maximo 30.');
+	} else if (habitaciones.length >= 1 || habitaciones.length <= 4) {
+		alert('Las habitaciones solo acepta como minimo 1 y maximo 4.');
 		return false;
-	} else if (!regexnombre.test(nombre)) {
+	} else if (cocinas.length >= 1 || cocinas.length <= 3) {
+		alert('Las cocinas solo acepta como minimo 1 y maximo 3.');
+		return false;
+	} else if (lavanderia.length >= 1 || lavanderia.length <= 4) {
+		alert('Lavanderia solo acepta como minimo 1 y maximo 4');
+		return false;
+	} else if (sala.length >= 1 || sala.length <= 3) {
+		alert('La Cantidad de salas solo acepta com minimo 1 y maximo 3.');
+		return false;
+	} else if (baño.length >= 1 || cocinas.length <= 3) {
+		alert('La cantidad de Baños solo acepta como min 1 y max 3.');
+		return false;
+	} else if (tamaño.length < 4 || tamaño.length >= 10) {
+		alert('El tamaño de Departamento es de 4 digitos como min y 10 max');
+		return false;
+	} else if (!regextamaño.test(tamaño)) {
 		alert('Ingrese solo letras.');
-		return false;
-	} else if (apellido.length < 3 || apellido.length >= 30) {
-		alert('El apellido paterno solo acepta como minimo 3 y maximo 30.');
-		return false;
-	} else if (!regexnombre.test(apellido)) {
-		alert('Ingrese solo un apellido paterno, tampoco se acepta espacios en blanco.');
-		return false;
-	} else if (dni.length < 8 || dni.length > 8) {
-		alert('El dni no tiene que ser menor a 8 o mayor a 8 digitos.');
-		return false;
-	} else if (celular.length < 9 || celular.length > 9) {
-		alert('El celular  no tiene que ser menor a 9 digitos.');
-		return false;
-	} else if (correo.length < 20 || correo.length >= 50) {
-		alert('El correo solo acepta como minimo 20 y maximo 50.');
-		return false;
-	} else if (!regexemail.test(correo)) {
-		alert('El correo no es valido, ejemplo : example123@example.com');
-		return false;
-	} else if (password.length < 8 || password.length >= 20) {
-		alert('El password solo acepta como minimo 8 y maximo 20.');
-		return false;
-	}else if(isNaN(descripcionRaza)){
-		alert("Ingrese solo letras en la descripción de mascota.");
 		return false;
 	}
 
