0340eddb8cc1b47363186d0ef164ce8b35041d6c
brandon708 <sotomallquibrandon708@gmail.com>
2022-04-19 19:25:51 -0500

    Acabado listado

--- src/main/java/project/departamento/com/controller/CaracteristicasDepartamentoController.java
+++ src/main/java/project/departamento/com/controller/CaracteristicasDepartamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class CaracteristicasDepartamentoController {
 	private CaracteristicasDepartamentoService service;
 	
 	@RequestMapping("/")
-	public String Index() {
+	public String Index(Model model) {
+		model.addAttribute("bean", service.listarCaracteristicaDepartamento());
 		return "departamento";
 	}
 	@GetMapping
