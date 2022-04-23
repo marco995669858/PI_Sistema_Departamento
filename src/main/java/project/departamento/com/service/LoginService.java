87c8b598c542872485cb6184f80191f46067a43f
Julio23-ciber <fenixxd23@gmail.com>
2022-04-22 21:36:43 -0500

    "Creacion del Login por Echevarria"

---
+++ src/main/java/project/departamento/com/service/LoginService.java
@@ -0,0 +1,9 @@
+package project.departamento.com.service;
+
+import project.departamento.com.entity.Usuario;
+
+public interface LoginService {
+	
+	public abstract Usuario buscarUsuario(String gmail);
+	
+}
