4959f734eb0edd30e64532d659229b3c1082e3fd
marco995669858 <marco995669858@gmail.com>
2022-05-03 15:57:33 -0500

    retoques

--- src/main/java/project/departamento/com/controller/MascotasController.java
+++ src/main/java/project/departamento/com/controller/MascotasController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import project.departamento.com.service.MascotasService;
-
 
 
 @Controller
