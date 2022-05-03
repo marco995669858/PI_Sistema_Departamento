f5aa381eeb406bd5942e4a2e47984f2f4195ad36
LeonardoDiaz21 <ldiazcanales22@gmail.com>
2022-05-03 17:45:37 -0500

    listado

--- src/main/java/project/departamento/com/controller/PropietariodepController.java
+++ src/main/java/project/departamento/com/controller/PropietariodepController.java
@@ -1,12 +1,16 @@
 package project.departamento.com.controller;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import project.departamento.com.entity.Propietariodep;
@@ -20,10 +24,21 @@ public class PropietariodepController {
 	private PropietariodepService service;
 
 	@RequestMapping("/")
-	public String index() {
+	public String Index(Model model) {
+		model.addAttribute("listado", listarPropietario());
 		return "Propietario";
 	}
 
+	@GetMapping
+	@ResponseBody
+	public List<Propietariodep> listarPropietario() {
+		return service.listarPropietariosdep();
+	}
+	
+	
+	
+	
+	
 	@RequestMapping("/registra_actualiza")
 	public String registra_actualizar_propietariodep(@RequestParam("codigo") int codigo,
 			@RequestParam("nombres") String nombres, @RequestParam("apePaterno") String apePaterno,
