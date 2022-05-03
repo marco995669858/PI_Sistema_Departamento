33bdb6a8851385523d8599d07c72afbc4b157dd9
marco995669858 <marco995669858@gmail.com>
2022-05-03 16:14:12 -0500

    ajustes

--- src/main/java/project/departamento/com/controller/DepartamenoController.java
+++ src/main/java/project/departamento/com/controller/DepartamenoController.java
@@ -1,13 +1,11 @@
 package project.departamento.com.controller;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -15,7 +13,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import project.departamento.com.entity.Departamento;
 import project.departamento.com.entity.Edificio;
 import project.departamento.com.entity.EstadoDepartamento;
-import project.departamento.com.entity.Propietariodep;
 import project.departamento.com.entity.TipoDepartamento;
 import project.departamento.com.service.DepartamentoService;
 
--- src/main/java/project/departamento/com/controller/OcupanteController.java
+++ src/main/java/project/departamento/com/controller/OcupanteController.java
@@ -1,7 +1,6 @@
 package project.departamento.com.controller;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import project.departamento.com.entity.Departamento;
-import project.departamento.com.entity.Edificio;
-import project.departamento.com.entity.EstadoDepartamento;
 import project.departamento.com.entity.Ocupantes;
-import project.departamento.com.entity.Propietariodep;
-import project.departamento.com.entity.TipoDepartamento;
-import project.departamento.com.service.DepartamentoService;
 import project.departamento.com.service.OcupanteService;
 
 @Controller
