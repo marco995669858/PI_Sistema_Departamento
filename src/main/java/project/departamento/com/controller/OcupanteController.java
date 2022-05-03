62a05750d472d13efad71b2475f2cd1f9c1ec37e
FernandoXYXY <fernandoxyxy2706@gmail.com>
2022-05-03 04:28:50 -0500

    clasesocupantes

--- src/main/java/project/departamento/com/controller/OcupanteController.java
+++ src/main/java/project/departamento/com/controller/OcupanteController.java
@@ -1,5 +1,24 @@
 package project.departamento.com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import project.departamento.com.service.DepartamentoService;
+import project.departamento.com.service.OcupanteService;
+
+@Controller
+@RequestMapping("/registro/ocupante")
 public class OcupanteController {
 
+	@Autowired
+	private OcupanteService service;
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("departamento", service.listarDepartamentos());
+		model.addAttribute("tipodepartamento", service.listarRelacionconPropietario());
+		return "departamento";
+	}
 }
--- src/main/java/project/departamento/com/service/OcupanteService.java
+++ src/main/java/project/departamento/com/service/OcupanteService.java
@@ -1,5 +1,24 @@
 package project.departamento.com.service;
 
-public interface OcupanteService {
+import java.util.List;
 
+import project.departamento.com.entity.Departamento;
+import project.departamento.com.entity.Ocupantes;
+import project.departamento.com.entity.RelacionconPropietario;
+
+public interface OcupanteService {
+/*para listar los ocupantes*/
+	public List<Ocupantes> listarOcupantes();
+	
+	/*para listar los departamentos*/
+	public List<Departamento> listarDepartamentos();
+	
+	
+	/*para listar la relacion*/
+	public List<RelacionconPropietario> listarRelacionconPropietario();
+	
+	
+	/*para registrar llos departamentos*/
+	public void registra_actualiza_ocupante(Ocupantes ocupante);
+	
 }
