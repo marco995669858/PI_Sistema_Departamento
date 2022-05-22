7599b236448254c7ddfd0fbefc2e958bab31c699
marco995669858 <92277173+marco995669858@users.noreply.github.com>
2022-05-21 19:51:34 -0500

    anvanze

---
+++ src/main/java/project/departamento/com/repository/RolRepository.java
@@ -0,0 +1,9 @@
+package project.departamento.com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.departamento.com.entity.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+}
